c5813b3aa7a811355dccea99bb7c4b111f43bcf4
Andrew Blair <ablair6254@stevenscollege.edu>
2024-10-21 20:40:37 -0400

    Code for the solution and a project

---
+++ helloWorld/helloWorld code.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Check if a name was provided as a command-line argument
+            string name = args.Length > 0 ? args[0] : "user";
+
+            // Output the greeting
+            Console.WriteLine($"Hi from CNSA 212, {name}!");
+        }
+    }
+}
