91954cb5c38ec84d708be87065bc2a85a1facd6a
Andrew Blair <ablair6254@stevenscollege.edu>
2024-10-22 19:02:32 -0400

    Reimplementing echo badly

---
+++ Echo/Reimplementing echo badly.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Echo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // If no arguments are provided, do nothing.
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No input provided.");
+                return;
+            }
+
+            // Check if the first argument is 'R' for reverse
+            if (args[0].ToUpper() == "R")
+            {
+                // Reverse the arguments excluding the first 'R'
+                Array.Reverse(args, 1, args.Length - 1);
+            }
+
+            // Print out the arguments either normal or reversed
+            Console.WriteLine(string.Join(" ", args.Skip(1)));
+        }
+    }
+}
